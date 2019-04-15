cba62852ac92ccf348eec421da40c1eb6b126c53
Lei Qin <lisalqin@gmail.com>
2019-04-15 18:36:04 -0500

    edit

--- sakila.sql
+++ sakila.sql
@@ -60,7 +60,11 @@ UPDATE actor
 SET first_name = 'HARPO'
 WHERE first_name = 'GROUCHO' and last_name = 'WILLIAMS';
 
+SET SQL_SAFE_UPDATES = 1;
+
 -- * 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct name after all! In a single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`.
+SET SQL_SAFE_UPDATES = 0;
+
 UPDATE actor
 SET first_name = 'GROUCHO'
 WHERE first_name = 'HARPO' and last_name = 'WILLIAMS';
